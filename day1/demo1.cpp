022f6b59d83727918ad53a6ab00941368ba36aff
MiChongGET <1564666023@qq.com>
2018-09-12 00:15:18 +0800

    交换数值

--- day1/demo1.cpp
+++ day1/demo1.cpp
@@ -1,5 +1,6 @@
 /*
  *  引用作为函数参数案例
+ *  对于C语言的指针来说，引用更加简单和方便
  */
 #include <iostream>
 using namespace std;
@@ -22,8 +23,25 @@ int main()
     stu.age = 10;
     //此时age已经被修改为20了
     test1(stu);
-
     cout << "学生的年龄是：" << stu.age << endl;
+
+    //交换数值
+    int a = 10;
+    int b = 20;
+    swap(a, b);
+    cout << "交换之后的数值" << endl;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+
     system("pause");
     return 0;
+}
+
+//交换两个变量的数值
+void swap(int &i, int &j)
+{
+    int temp;
+    temp = i;
+    i = j;
+    i = temp;
 }
