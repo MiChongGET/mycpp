44ca2f878bdd07761d9ffb33cd3dc077b5e1c3c6
MiChongGET <1564666023@qq.com>
2018-09-11 23:27:50 +0800

    引用案例

--- day1/demo1.cpp
+++ day1/demo1.cpp
@@ -1,3 +1,6 @@
+/*
+ *  引用作为函数参数案例
+ */
 #include <iostream>
 using namespace std;
 
@@ -7,15 +10,20 @@ struct Student
     char name[20];
 };
 
-int main()
-{
-
-    // test1
-    
-    return 0;
-}
-
 void test1(struct Student &s)
 {
+    //相当于把s直接传入进来，对s直接修改
     s.age = 20;
+}
+
+int main()
+{
+    Student stu;
+    stu.age = 10;
+    //此时age已经被修改为20了
+    test1(stu);
+
+    cout << "学生的年龄是：" << stu.age << endl;
+    system("pause");
+    return 0;
 }
