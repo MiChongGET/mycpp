242bdc950272c9e1876a8a9186b11f63efde1a45
MiChongGET <1564666023@qq.com>
2018-09-16 17:08:53 +0800

    判断两个圆的位置

--- day4/AdCircle.cpp
+++ day4/AdCircle.cpp
@@ -45,11 +45,24 @@ class Circle
         circle_r = r;
     }
 
+    double getX()
+    {
+        return circle_x;
+    }
+    double getY()
+    {
+        return circle_y;
+    }
+    double getR()
+    {
+        return circle_r;
+    }
+
     //判断点关于圆的位置
     void judgePosition(Point &p)
     {
-        int distance;
-        int r = circle_r * circle_r;
+        double distance;
+        double r = circle_r * circle_r;
 
         distance = (circle_x - p.getX()) * (circle_x - p.getX()) + (circle_y - p.getY()) * (circle_y - p.getY());
         if (r > distance)
@@ -65,6 +78,28 @@ class Circle
             cout << "在圆外" << endl;
         }
     }
+    //判断两个圆的位置
+    void circlePersion(Circle &circle)
+    {
+        //圆心距离平方
+        double circle_point_distance;
+        //半径之和平方
+        double circle_r_long = (circle_r + circle.getR()) * (circle_r + circle.getR());
+        circle_point_distance = (circle.getX() - circle_x) * (circle.getX() - circle_x) + (circle.getY() - circle_y) * (circle.getY() - circle_y);
+
+        if (circle_r_long > circle_point_distance)
+        {
+            cout << "两个圆相交" << endl;
+        }
+        else if (circle_r_long == circle_point_distance)
+        {
+            cout << "两个圆相切" << endl;
+        }
+        else if (circle_r_long < circle_point_distance)
+        {
+            cout << "两个圆相离" << endl;
+        }
+    }
 };
 
 int main()
@@ -77,6 +112,10 @@ int main()
     Point point;
     point.setXY(20, 30);
     circle.judgePosition(point);
+
+    Circle circle2;
+    circle2.setXY(2, 3, 4);
+    circle2.circlePersion(circle);
 
     system("pause");
     return 0;
