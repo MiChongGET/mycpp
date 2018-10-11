f9e761024929bfe089d60d396d922d78701a02ed
MiChongGET <1564666023@qq.com>
2018-10-11 23:02:57 +0800

    对象序列化

--- day12/二进制文件（对象序列化）.cpp
+++ day12/二进制文件（对象序列化）.cpp
@@ -18,6 +18,7 @@ class Teacher
     }
     void printT()
     {
+        cout << "name:" << name << "-----age:" << age << endl;
     }
 
   protected:
@@ -33,8 +34,25 @@ int main()
     if (!ofs)
     {
         cout << "文件打开失败！" << endl;
-        return;
+        return 0;
     }
+
+    Teacher t1(18, "MiChong");
+    Teacher t2(22, "赵晓东");
+
+    //将对象信息写入到文件中去
+    ofs.write((char *)&t1, sizeof(Teacher));
+    ofs.write((char *)&t2, sizeof(Teacher));
+
+    // 从文件中读取序列化后的信息
+    Teacher temp;
+
+    ifstream ifs("d:\\target.txt", ios::in);
+    ifs.read((char *)&temp, sizeof(Teacher));
+    temp.printT();
+
+    ifs.read((char *)&temp, sizeof(Teacher));
+    temp.printT();
 
     system("pause");
     return 0;
